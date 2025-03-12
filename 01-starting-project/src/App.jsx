c9c0a5413caa3d10b5245741354ad10544a4eea8
sisun0070 <jisun5812@naver.com>
2025-03-12 21:48:46 +0900

    feat:202503123 react page - props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,39 +1,30 @@
+// import Header from "./components/Header";
+// import 이미지변수명 from "경로";
+import Header2 from "./components/Header/Header2";
+// import componentsImg from "./assets/components.png"
 
-const reactDescriptionsKor = [ '근본적인', '결정적인', '핵심적인'];
-const reactDescriptionsEng = ['Fundamental', 'Crucial', 'Core' ];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));   // 얘의 return값은 0 ~ 2의 int가 반환됨
-  // 어떻게? Math.random() * (max + 1) 얘의 범위가 0.xxxx ~ 2.xxxx이기 때문
-}
-
-function Header () {
-  return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React 필수 요소 복습</h1>
-        <p>
-          {reactDescriptionsKor[getRandomInt(2)]} React concepts you will need for almost any app you are
-          going to build!
-
-          <br />
-          대부분의 앱을 위해 당신이 필요로 할 {reactDescriptionsKor[getRandomInt(2)]} React 개념 학습 😀
-        </p>
-      </header>
+function CoreConcepts(props) {
+  return(
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
 function App() {
   return (
     <div>
-        <Header />
-        <main>
-            <h2>Time to get started!</h2>
-
-            <br />
-
-            <h2>시작해봅시다!🎈</h2>
-        </main>
+      <Header2 />
+      <main>
+        <section id="core-concepts">
+          <h2>핵심 개념들 of React</h2>
+          <ul>
+            <CoreConcepts image={componentsImg} title={props.Components} description={props.description} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
