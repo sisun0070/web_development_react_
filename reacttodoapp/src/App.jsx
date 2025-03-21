e99d1869d308bb3ce7f707bfabaeb3f47095b8d8
sisun0070 <jisun5812@naver.com>
2025-03-21 21:34:20 +0900

    feat: 20250321 spread syntax / .map() method / todolist

--- reacttodoapp/src/App.jsx
+++ reacttodoapp/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ toDo, setTodo ] = useState('');
+  const [ toDos, setToDos ] = useState([]);
+
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === '') {
+      return;
+    }
+
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setTodo(''); 
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          onChange={onChange}
+          value={toDo}
+          placeholder="Type Your To Do 💻"/>
+        <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+          {toDos.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+    </div>
   )
 }
 
